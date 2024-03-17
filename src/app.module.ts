42cc39ff6fdede0e47101271f265d314bd2e7559
thewamkov <thewankov@gmail.com>
2024-03-17 13:33:12 +0200

    fixed db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from './database/database.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import typeorm from './config/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [DatabaseModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeorm],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const options = await configService.get('typeorm');
+
+        if (!options) {
+          throw new Error('TypeORM options are undefined');
+        }
+        return options;
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
